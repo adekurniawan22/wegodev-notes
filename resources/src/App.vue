c7325016db3953180c606c82b8ba291053e4861f
Ade Kurniawan <57614206+adekurniawan22@users.noreply.github.com>
2023-12-27 22:40:35 +0700

    tes

---
+++ resources/src/App.vue
@@ -0,0 +1,193 @@
+<template>
+  <div id="app">
+    <div class="kiri">
+      <div class="logo">
+        <a href="#">
+          <h2>Wegodev</h2>
+          <span>notes</span>
+        </a>
+      </div>
+      <div class="frame-notes">
+        <button @click="newNotes" class="bg-success btn btn-new-note">
+          + Note Baru
+        </button>
+
+        <ListNotes :propNotes="notes" :propEditNote="editNote" />
+      </div>
+    </div>
+    <div class="kanan">
+      <FormNotes
+        :propSaveNote="saveNote"
+        :propRemoveNote="removeNote"
+        :propUpdateNote="updateNote"
+        :propDataForm="dataForm"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import ListNotes from "./components/listNotes.vue";
+import FormNotes from "./components/formNotes.vue";
+
+export default {
+  name: "App",
+  data: function () {
+    return {
+      dataForm: {},
+      notes: [
+        {
+          id: 1,
+          title: "Wegodev",
+          description: "Ini isi wegodev",
+        },
+        {
+          id: 2,
+          title: "Super User",
+          description: "Ini isi super user",
+        },
+      ],
+    };
+  },
+  components: {
+    ListNotes,
+    FormNotes,
+  },
+  methods: {
+    newNotes() {
+      this.dataForm = { id: 0, title: "", description: "" };
+    },
+    editNote(id) {
+      this.dataForm = this.notes.find((note) => note.id === id);
+    },
+    saveNote(title, description) {
+      let newId = 0;
+      if (this.notes.length === 0) {
+        newId = 1;
+      } else {
+        newId = this.notes[this.notes.length - 1].id + 1;
+      }
+
+      let newNote = { id: newId, title: title, description: description };
+      this.notes.push(newNote);
+      this.editNote(newId);
+    },
+    updateNote(id, title, description) {
+      let noteIndex = this.notes.findIndex((note) => note.id === id);
+      this.notes[noteIndex].title = title;
+      this.notes[noteIndex].description = description;
+    },
+    removeNote(id) {
+      let noteIndex = this.notes.findIndex((note) => note.id === id);
+      this.notes.splice(noteIndex, 1);
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0px;
+  overflow: hidden;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  padding: 0px;
+
+  display: flex;
+  width: 100%;
+}
+
+.kiri {
+  width: 400px;
+  background: #f7f7f7;
+  color: #616161;
+}
+.logo {
+  padding: 25px 15px;
+  border-bottom: 1px solid gainsboro;
+}
+.logo a {
+  text-decoration: none;
+}
+.logo a h2 {
+  margin: 0px;
+  display: inline;
+  margin-right: 5px;
+  font-size: 35px;
+  text-transform: capitalize;
+  color: #757575;
+}
+.logo a span {
+  font-size: 12px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #139e5f;
+}
+.frame-notes {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 85vh;
+}
+.bg-success {
+  background: #219a63;
+  color: white;
+  outline: none;
+}
+.bg-success:hover {
+  background: #24b774;
+  color: white;
+}
+.bg-danger {
+  background: #b50000;
+  color: white;
+}
+.bg-danger:hover {
+  background: #c50000;
+}
+.btn {
+  border: none;
+  font-size: 12px;
+  text-align: center;
+  letter-spacing: 1px;
+  cursor: pointer;
+  border-radius: 2px;
+  padding: 7px 25px;
+  outline: none;
+}
+.btn-new-note {
+  width: 90%;
+  padding: 12px 10px;
+  margin: 10px 15px;
+  text-align: left !important;
+}
+
+.kanan {
+  width: 100%;
+  overflow-y: scroll;
+  height: 100vh;
+  border-left: 1px solid gainsboro;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f7f7f7;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
---
+++ resources/src/components/formNotes.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="formNotes">
+    <form @submit="submitNote">
+      <div class="menu">
+        <button
+          type="button"
+          @click="submitRemove"
+          class="bg-danger btn btn-delete"
+        >
+          Delete
+        </button>
+        <button type="submit" class="bg-success btn">Save</button>
+      </div>
+      <div class="content">
+        <input type="text" class="text" placeholder="id" v-model="id" />
+        <input type="text" class="text" placeholder="Title" v-model="title" />
+        <textarea
+          class="text textarea"
+          placeholder="Tuliskan rencana kamu"
+          v-model="description"
+        ></textarea>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "formNotes",
+  props: {
+    propSaveNote: {
+      type: Function,
+    },
+    propUpdateNote: {
+      type: Function,
+    },
+    propRemoveNote: {
+      type: Function,
+    },
+    propDataForm: {
+      type: Object,
+    },
+  },
+  data: function () {
+    return { id: 0, title: "", description: "" };
+  },
+  methods: {
+    submitNote(e) {
+      e.preventDefault();
+      if (this.id === 0) {
+        this.propSaveNote(this.title, this.description);
+      } else {
+        this.propUpdateNote(this.id, this.title, this.description);
+      }
+    },
+    submitRemove() {
+      this.propRemoveNote(this.id);
+      this.id = 0;
+      this.title = "";
+      this.description = "";
+    },
+  },
+  watch: {
+    propDataForm: function (note) {
+      this.id = note.id;
+      this.title = note.title;
+      this.description = note.description;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.menu {
+  background: #f7f7f7;
+  padding: 10px 25px;
+  margin-bottom: 25px;
+  text-align: right;
+  border-bottom: 1px solid #e8e6e6;
+}
+.btn-delete {
+  margin-right: 10px;
+}
+.content {
+  padding: 0px 25px;
+}
+.text {
+  display: block;
+  width: 100%;
+  padding: 0px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #2c3e50;
+  border: none;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  outline: none;
+}
+.textarea {
+  min-height: 350px;
+  font-size: 15px;
+  font-weight: lighter;
+  line-height: 30px;
+}
+.loader {
+  vertical-align: middle;
+}
+</style>
